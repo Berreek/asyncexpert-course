1469f12805ed32148c88a17b5f364ed38266762d
Bartosz Behring <bartosz@spotio.com>
2020-10-11 20:05:47 +0200

    Implement another excercise

--- Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
+++ Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
@@ -8,7 +8,31 @@ namespace TaskCompletionSourceExercises.Core
     {
         public static Task<string> RunProgramAsync(string path, string args = "")
         {
-            return Task.FromResult(string.Empty);
+            var process = new Process
+            {
+                EnableRaisingEvents = true,
+                StartInfo = new ProcessStartInfo(path)
+                {
+                    RedirectStandardOutput = true, RedirectStandardError = true, Arguments = args
+                }
+            };
+
+            var tcs = new TaskCompletionSource<string>();
+            process.Exited += async (sender, eventArgs) =>
+            {
+                var senderProcess = sender as Process;
+                var error = await senderProcess?.StandardError?.ReadToEndAsync();
+                if (!string.IsNullOrEmpty(error))
+                {
+                    tcs.SetException(new Exception(error));
+                }
+                
+                tcs.SetResult(await senderProcess?.StandardOutput?.ReadToEndAsync());
+                senderProcess?.Dispose();
+            };
+            process.Start();
+            
+            return tcs.Task;
         }
     }
 }
